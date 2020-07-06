13c6201783b381d9ad50632d895d8456264e8344
bharatvaliv <bharat.valiv@gmail.com>
2020-07-07 00:22:00 +0530

    movie modal created

---
+++ moviebooking/src/app/movies/movie.modal.ts
@@ -0,0 +1,22 @@
+export interface seats_cost {
+  seats: number;
+  cost: number;
+}
+
+export interface total_seats {
+  platinum: seats_cost;
+  gold: seats_cost;
+  silver: seats_cost;
+}
+
+export interface Movie {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  total_seats: total_seats;
+  schedule: {
+    time: string;
+    seatsOccupied: number[];
+  };
+}
